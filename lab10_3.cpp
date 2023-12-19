9d0e7a5fbef399407492854a879754c8b11e7871
OrneryCash <60810240+OrneryCash@users.noreply.github.com>
2023-12-19 15:35:06 +0700

    lab10_3.cpp completed

--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,3 +1,33 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+  int n = 0;
+  float sum = 0;
+  float squaredSum = 0;
+
+  string score;
+  ifstream file("score.txt");
+  while (getline(file, score)) {
+    n++;
+    float s = stof(score);
+    sum += s;
+    squaredSum += s * s; 
+  }
+
+  file.close();
+
+  float mean = sum / n;
+  float squaredMean = squaredSum / n; 
+
+  float stdi = sqrt(squaredMean - mean*mean);
+  cout << "Number of data = " << n << '\n';
+  cout << setprecision(3);
+  cout << "Mean = " << mean << '\n';
+  cout << "Standard deviation = " << stdi; 
+}
+
